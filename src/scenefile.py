25939e36ec41e07cc86b1fe2af378afa111a1cc1
gerardoReyes117 <80165635+gerardoReyes117@users.noreply.github.com>
2021-03-09 20:42:21 -0600

    Increment Save
    Adds +1 to save version number. Checks for matching filetype if multiple files with same version scheme are in the same folder. Checks for a version number higher and adds +1 to that one instead.

--- src/scenefile.py
+++ src/scenefile.py
@@ -1,7 +1,7 @@
 import logging
+
 import pymel.core as pmc
 from pymel.core.system import Path
-
 
 log = logging.getLogger(__name__)
 
@@ -54,6 +54,35 @@ class SceneFile(object):
             log.warning("Missing directories in path. Creating directories...")
             self.folder_path.makedirs_p()
             return pmc.system.saveAs(self.path)
+
+    def next_avail_ver(self):
+        """Return the next available version number in this folder."""
+        pattern = "{descriptor}_{task}_v*{ext}".format(
+            descriptor=self.descriptor, task=self.task, ext=self.ext)
+        matching_scenefiles = []
+        for file_ in self.folder_path.files():
+            if file_.name.fnmatch(pattern):
+                matching_scenefiles.append(file_)
+        if not matching_scenefiles:
+            return 1
+        matching_scenefiles.sort(reverse=True)
+        latest_scenefile = matching_scenefiles[0]
+        latest_scenefile = latest_scenefile.name.stripext()
+        latest_ver_num = int(latest_scenefile.split("_v")[-1])
+        return latest_ver_num + 1
+
+    def increment_save(self):
+        """Increments the version and saves the scene file.
+
+        If the existing version of a file already exists, it should increment
+        from the largest version number available in the folder.
+
+        Returns:
+            Path: the path to the scene file if successful
+        """
+        self.ver = self.next_avail_ver()
+        self.save()
+
 
 # scene_file = scenefile.SceneFile("D:\\School Files\\spring 2021\\sandbox\\tank_model_v001.ma")
 # paste to maya
