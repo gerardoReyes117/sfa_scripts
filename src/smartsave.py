83a9d9357784cbd070dca6ec3ae71bf0c3e1d3d8
gerardoReyes117 <80165635+gerardoReyes117@users.noreply.github.com>
2021-03-29 02:47:53 -0500

    defined ui
    Created a Qlabel set to "title" within a Box Layout set as the main layout for the smartsave ui window.

--- src/smartsave.py
+++ src/smartsave.py
@@ -25,6 +25,13 @@ class SmartSaveUI(QtWidgets.QDialog):
         self.setMaximumHeight(200)
         self.setWindowFlags(self.windowFlags() ^
                             QtCore.Qt.WindowContextHelpButtonHint)
+        self.create_ui()
+
+    def create_ui(self):
+        self.title_lbl = QtWidgets.QLabel("SmartSave")
+        self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addWidget(self.title_lbl)
+        self.setLayout(self.main_lay)
 
 
 class SceneFile(object):
