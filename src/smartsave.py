76c8429eb04c64052928c2a7b2facc5d72e31354
gerardoReyes117 <80165635+gerardoReyes117@users.noreply.github.com>
2021-03-29 03:43:27 -0500

    Refactored default folder into method
    Extracted method using pycharm into create folder ui and changed output to folder lay.

--- src/smartsave.py
+++ src/smartsave.py
@@ -31,6 +31,13 @@ class SmartSaveUI(QtWidgets.QDialog):
     def create_ui(self):
         self.title_lbl = QtWidgets.QLabel("SmartSave")
         self.title_lbl.setStyleSheet("font: bold 20px")
+        self.folder_lay = self._create_folder_ui()
+        self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addWidget(self.title_lbl)
+        self.main_lay.addLayout(self.folder_lay)
+        self.setLayout(self.main_lay)
+
+    def _create_folder_ui(self):
         default_folder = Path(cmds.workspace(rootDirectory=True, query=True))
         default_folder = default_folder / "scenes"
         self.folder_le = QtWidgets.QLineEdit(default_folder)
@@ -38,10 +45,7 @@ class SmartSaveUI(QtWidgets.QDialog):
         layout = QtWidgets.QHBoxLayout()
         layout.addWidget(self.folder_le)
         layout.addWidget(self.folder_browse_btn)
-        self.main_lay = QtWidgets.QVBoxLayout()
-        self.main_lay.addWidget(self.title_lbl)
-        self.main_lay.addLayout(layout)
-        self.setLayout(self.main_lay)
+        return layout
 
 
 class SceneFile(object):
